12eb7e4002ea611b300ffa245454ab100d452f72
rovermore <robertomoreno@MacBook-Pro.local>
2019-03-12 22:40:58 +0100

    Maps added to detail activity

--- app/src/main/java/com/example/rovermore/planmad/activities/DetailActivity.java
+++ app/src/main/java/com/example/rovermore/planmad/activities/DetailActivity.java
@@ -3,13 +3,22 @@ package com.example.rovermore.planmad.activities;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.example.rovermore.planmad.R;
 import com.example.rovermore.planmad.datamodel.Event;
 import com.example.rovermore.planmad.fragments.DetailFragment;
 import com.example.rovermore.planmad.fragments.MainFragment;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private static final String TAG = DetailActivity.class.getSimpleName();
 
     private Event event;
     private FragmentManager fragmentManager;
@@ -30,5 +39,28 @@ public class DetailActivity extends AppCompatActivity {
         fragmentManager.beginTransaction()
                 .replace(R.id.detail_frame_layout, detailFragment)
                 .commit();
+
+        // Get the SupportMapFragment and request notification
+        // when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        LatLng eventLocation = new LatLng(event.getLatitude(), event.getLongitude());
+        googleMap.addMarker(new MarkerOptions().position(eventLocation)
+                .title(event.getEventLocation()));
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+        googleMap.getUiSettings().setZoomGesturesEnabled(false);
+        googleMap.getUiSettings().setScrollGesturesEnabled(false);
+        googleMap.getUiSettings().setTiltGesturesEnabled(false);
+        googleMap.getUiSettings().setScrollGesturesEnabledDuringRotateOrZoom(false);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(eventLocation,16.0f));
+        Log.d(TAG,"CODE IS EXECUTING " + event.getEventLocation());
     }
 }
